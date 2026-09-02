dfe9157bf7d5f84d348bf48873832bef14082c19
B Geo 22 <161180938+Geo222222@users.noreply.github.com>
2026-09-02 13:17:47 -0500

    feat: activate integrated client polish

--- apps/client-console/src/main.tsx
+++ apps/client-console/src/main.tsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { App } from './App';
+import { installProductPolish } from './polish';
 import './styles.css';
 import './theme.css';
+import './polish.css';
+
+installProductPolish();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
